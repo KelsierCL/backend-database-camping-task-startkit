648e9d276bf8569003e945508e37d6539f74d0d6
Rayne <gelin90075@gmail.com>
2024-11-30 17:51:46 +0800

    update 1-5

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -52,6 +52,9 @@ from "USER";
 
 -- 1-5 查詢：取得 USER 資料表所有用戶資料，並列出前 3 筆（提示：使用limit語法）
 
+SELECT id, name, email, role, created_at, updated_at
+FROM "USER"
+limit 3;
 
 --  ████████  █████   █    ████  
 --    █ █   ██    █  █         █ 
